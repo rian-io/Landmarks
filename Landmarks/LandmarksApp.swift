f64ccac3d98874d340fa23b4bf085abc3bd9b7b2
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2021-12-15 19:28:50 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rian Oliveira on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
